b93e65550873c3798746f4a0f89aa58ffefa3610
Aoshi_F <blue_20will@yahoo.co.jp>
2016-05-13 08:17:17 +0000

    add validation, edit, some modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
+  validates :location, length: { maximum: 30 }
+  validates :hobby, length: { maximum: 50 }
+  validates :introduce, length: { maximum: 200 }
+  
   has_secure_password
 end
